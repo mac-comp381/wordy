07a4ae5ca479e0d85a1201843920547cde676ac8
monsieurjonees <phil.reitzjones@me.com>
2024-09-18 17:34:44 -0500

    Compiler in progress

--- src/wordy/ast/BinaryExpressionNode.java
+++ src/wordy/ast/BinaryExpressionNode.java
@@ -1,5 +1,6 @@
 package wordy.ast;
 
+import java.io.PrintWriter;
 import java.util.Map;
 import java.util.Objects;
 
@@ -84,6 +85,42 @@ public class BinaryExpressionNode extends ExpressionNode {
         
             default:
                 return 0;
+        }
+    }
+
+    @Override
+    public void compile(PrintWriter out) {
+        if (operator == Operator.EXPONENTIATION) {
+            out.print("Math.pow(");
+            lhs.compile(out);
+            out.print(", ");
+            rhs.compile(out);
+            out.print(")");
+        } else {
+            out.print("(");
+            lhs.compile(out);
+            switch (operator) {
+                case ADDITION:
+                    out.print(" + ");
+                    break;
+            
+                case SUBTRACTION:
+                    out.print(" - ");
+                    break;
+            
+                case MULTIPLICATION:
+                    out.print(" * ");
+                    break;
+            
+                case DIVISION:
+                    out.print(" / ");
+                    break;
+            
+                default:
+                    break;
+            }
+            rhs.compile(out);
+            out.print(")");
         }
     }
 }
